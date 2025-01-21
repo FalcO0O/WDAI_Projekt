a065f1f855d9ce13a5c3405f81c2ee23d0b26467
tomaszmol <tmollegro@gmail.com>
2025-01-21 12:14:49 +0100

    postawienie MUI

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Roboto, Helvetica, Arial, sans-serif",
+  },
+  palette: {
+    primary: {
+      main: "#1976d2", // Customize your primary color
+    },
+    secondary: {
+      main: "#dc004e", // Customize your secondary color
+    },
+  },
+});
+
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function

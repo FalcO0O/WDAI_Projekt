7f4cd45a13c75507ec2ba11857373deeec0df447
tomaszmol <tmollegro@gmail.com>
2025-01-25 22:48:50 +0100

    dodanie kalendarza z wyborem dat

--- app/src/routes/FutureMealsPlanner.tsx
+++ app/src/routes/FutureMealsPlanner.tsx
@@ -4,10 +4,13 @@ import Footer from "../components/Footer";
 import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import { Box, Button, Typography } from "@mui/material";
-import { dateContainerStyle } from "../styles/style";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
 function FutureMealsPlanner() {
   const [currentDate, setCurrentDate] = useState(new Date()); // Ustawienie daty początkowej
+  const [openCalendar, setOpenCalendar] = useState(false);
 
   const formattedDate = currentDate.toLocaleDateString("pl-PL", {
     weekday: "long",
@@ -30,6 +33,14 @@ function FutureMealsPlanner() {
     setCurrentDate(previousDay);
   };
 
+  // Funkcja do obsługi zmiany daty przez użytkownika z kalendarza
+  const handleDateChange = (newDate: Date | null) => {
+    if (newDate) {
+      setCurrentDate(newDate);
+      setOpenCalendar(false); // Zamknij kalendarz po wyborze daty
+    }
+  };
+
   return (
     <div style={{ position: "relative" }}>
       <HomeBar />
@@ -47,7 +58,6 @@ function FutureMealsPlanner() {
         {/* Kontener na datę z oknem o stałej szerokości */}
         <Box
           sx={{
-            ...dateContainerStyle,
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
@@ -57,7 +67,9 @@ function FutureMealsPlanner() {
             borderRadius: "10px", // Zaokrąglone rogi
             boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)", // Cień dla okna
             position: "relative", // Aby strzałki były umieszczone względem tego okna
+            cursor: "pointer", // Kursor zmienia się na "pointer", aby pokazać, że można kliknąć
           }}
+          onClick={() => setOpenCalendar(true)} // Otwórz kalendarz po kliknięciu na datę
         >
           <Typography sx={{ fontSize: "1.2rem" }}>{formattedDate}</Typography>
         </Box>
@@ -98,6 +110,26 @@ function FutureMealsPlanner() {
           <ArrowRightIcon fontSize="inherit" />
         </Button>
       </Box>
+
+      {/* Kalendarz */}
+      {openCalendar && (
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DatePicker
+            open={openCalendar}
+            value={currentDate}
+            onChange={handleDateChange}
+            onClose={() => setOpenCalendar(false)} // Zamknij kalendarz po kliknięciu poza nim
+            sx={{
+              position: "absolute", // Ustawienie kalendarza na stałe na ekranie
+              top: "calc(80px + 50px)", // Pozycjonowanie kalendarza poniżej okna daty
+              left: "50%", // Wyrównanie do środka ekranu w poziomie
+              transform: "translateX(-50%)", // Przemieszczenie, aby był dokładnie na środku w poziomie
+              zIndex: 1300, // Upewnienie się, że kalendarz będzie nad innymi elementami
+            }}
+          />
+        </LocalizationProvider>
+      )}
+
       <Footer />
     </div>
   );
